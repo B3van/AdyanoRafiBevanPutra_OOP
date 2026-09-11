eb10d95f87655066214c4cc2e9e57f8e0f4972a2
adyanou <macbook@adyanou.local>
2026-09-11 11:34:24 +0700

    Add OOP Module 1 code

---
+++ Frontend_AdyanoRafiBevanPutra/core/src/main/java/com/Bevan/frontend/Enemy.java
@@ -0,0 +1,35 @@
+package com.Bevan.frontend;
+
+public class Enemy {
+    String name;
+    int hp, maxHP;
+
+    public Enemy(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHP = hp;
+    }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+
+        if (hp < 0) {
+            hp = 0;
+        }
+
+        System.out.println(name + " took " + damage + " damage! HP: " + hp + "/" + maxHP);
+
+        if (hp == 0) {
+            System.out.println(name + " was defeated!");
+        }
+    }
+
+    public void attack(Player target, int damage) {
+        System.out.println(name + " attacks " + target.name + " dealing " + damage + " DMG!");
+        target.takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+}
